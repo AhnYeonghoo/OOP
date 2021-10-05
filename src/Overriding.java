1ee803ba7e1ed59b70543308d718b6c93a504ec7
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-05 17:35:11 +0900

    super(),this()

--- src/Overriding.java
+++ src/Overriding.java
@@ -1,23 +1,35 @@
 class Point1 {
     int x;
     int y;
+    Point1() {
+        x = 0;
+        y = 0;
+    }
+
+    Point1(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     String getLocation() {
-        return "x :" + x + ", y : " + y;
+        return "x : " +x+ ", y : " + y;
     }
 }
-
 class Point3D extends Point1 {
     int z;
 
+    Point3D(int x, int y, int z) {
+        super(x,y); // Point1(int x, int y) 생성자 호출하는 super() 생성자
+        this.z = z;
+    }
     String getLocation() {
         return "x : " + x + ", y : " + y + ", z : " + z;
     }
 }
 public class Overriding {
     public static void main(String[] args) {
-        Point3D p3d = new Point3D();
-        p3d.x = 10; p3d.y = 20; p3d.z = 30;
-        System.out.println(p3d.getLocation());
+        Point3D p3 = new Point3D(1,2,3);
+        System.out.println(p3.getLocation());
 
     }
 }
