c46e7474758041d45f3b93e041e38fd27654136e
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-06 16:55:03 +0900

    Vector Class

--- src/PolyArgumentTest2.java
+++ src/PolyArgumentTest2.java
@@ -1,3 +1,4 @@
+
 class Product {
     int price;
     int bonusPoint;
@@ -6,6 +7,7 @@ class Product {
         this.price = price;
         bonusPoint = (int) (price / 10.0); //보너스포인트는 제품가격의 10%
     }
+    Product(){}
 }
 
 class TV extends Product {
@@ -22,9 +24,15 @@ class Computer extends Product {
     }
     public String toString() { return "Computer"; }
 }
+class Audio extends Product {
+    Audio() { super (50); }
+    public String toString() { return "Audio"; }
+}
 class Buyer {
     int money = 1000; // 소유금액
     int bonusPoint = 0; //보너스점수
+    Product[] item = new Product[10];
+    int i = 0;
 
     void buy(Product p) {
         if (money < p.price) {
@@ -33,16 +41,33 @@ class Buyer {
         }
         money -= p.price;
         bonusPoint += p.bonusPoint;
+        item[i++] = p;
         System.out.println(p + "을/를 구매하였습니다.");
     }
+    void summary() {
+        int sum = 0;
+        String itemList = "";
+
+        for (int i = 0; i < item.length; i++) {
+            if (item[i] == null) break;
+            sum += item[i].price;
+            itemList += (i==0) ? "" + item[i] : ", " + item[i];
+        }
+        System.out.println("구입하신 물품의 총 금액은 " + sum + " 입니다.");
+        System.out.println("구입하신 제품은 " + itemList+ "입니다.");
+    }
+    void show() {
+        System.out.println(item[0]);
+    }
 }
-public class PolyArgumentTest {
+
+class PolyArgumentTest2 {
     public static void main(String[] args) {
         Buyer b = new Buyer();
-
         b.buy(new TV());
         b.buy(new Computer());
-        System.out.println("현재 남은 돈은 " + b.money + "만원입니다.");
-        System.out.println("혅 보너스 점수는 "+ b.bonusPoint + "점입니다.");
+        b.buy(new Audio());
+        b.summary();
+
     }
 }
