b9bc76c00d8045eadc9b0270fc21df717a21cd18
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-07 21:22:45 +0900

    다형성 이해

--- src/Refresh.java
+++ src/Refresh.java
@@ -1,20 +1,43 @@
 public class Refresh {
-    public static void main(String[] args) {
-        String[] strArr = { "100" , "200", "300"};
-        System.out.println(concatenate("", "100", "200", "300"));
-        System.out.println(concatenate("-",strArr));
-        System.out.println(concatenate(",", new String[] {"1", "2", "3"} ));
-        System.out.println("["+concatenate(",", new String[0])+"]");
-
-
+    public static void func(People people) {
+        people.printInfo();
+        if(people instanceof Man)
+            ((Man)people).enlist();
+        if(people instanceof Woman)
+            ((Woman)people).makeUp();
     }
-    static String concatenate(String delim, String... args) {
-        String result = "";
-        for (String str : args) {
-            result += str + delim;
-        }
-        return result;
+    public static void main(String[] args) {
+        Man man = new Man();
+        Woman woman = new Woman();
+        func(man);
+        System.out.println();
+        func(woman);
+    }
+}
 
+class People {
+    public void printInfo() {
+        System.out.println("나는 사람입니다.");}
+}
+
+class Woman extends People {
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("그리고 여자입니다.");
+    }
+    public void makeUp() {
+        System.out.println("화장");
+    }
+}
+class Man extends People {
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("그리고 남자입니다.");
+    }
+    public void enlist() {
+        System.out.println("내일 군대를 갑니다.");
     }
 }
 
