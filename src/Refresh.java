e51050f7237508199163ec3dd9ea212f574c1eae
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-08 13:47:52 +0900

    인터페이스 이해

--- src/Refresh.java
+++ src/Refresh.java
@@ -1,43 +1,22 @@
 public class Refresh {
-    public static void func(People people) {
-        people.printInfo();
-        if(people instanceof Man)
-            ((Man)people).enlist();
-        if(people instanceof Woman)
-            ((Woman)people).makeUp();
-    }
-    public static void main(String[] args) {
-        Man man = new Man();
-        Woman woman = new Woman();
-        func(man);
-        System.out.println();
-        func(woman);
-    }
+
 }
 
-class People {
-    public void printInfo() {
-        System.out.println("나는 사람입니다.");}
-}
+interface Bank {
 
-class Woman extends People {
-    @Override
-    public void printInfo() {
-        super.printInfo();
-        System.out.println("그리고 여자입니다.");
-    }
-    public void makeUp() {
-        System.out.println("화장");
-    }
-}
-class Man extends People {
-    @Override
-    public void printInfo() {
-        super.printInfo();
-        System.out.println("그리고 남자입니다.");
-    }
-    public void enlist() {
-        System.out.println("내일 군대를 갑니다.");
-    }
-}
+    final int MAX_INTEGER = 10000000;
+    //인출메서드
+    void withDraw(int price);
+    //입금 메서드
+    void deposit(int price);
 
+    default String findDormancyAccount(String custId){
+        System.out.println("금즁개정법안");
+        System.out.println("금융결제원");
+        return "00은행 000-000-0000-00";
+    }
+    static void BCAuth(String bankName) {
+        System.out.println(bankName + "에서 블록체인");
+    }
+
+}
