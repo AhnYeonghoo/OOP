3a898afe6d62e21627eaea47a80f51e64ab5135d
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-14 20:17:55 +0900

    문자열 뒤집기

--- src/Exam.java
+++ src/Exam.java
@@ -1,27 +1,5 @@
 public class Exam {
     public static void main(String[] args) {
-        int [] sibal = new int[] {
-                89,92,61,76,94,84,77,62,87,58,58,83,78,64,77,77,61,79,66,64,79,32,77,60,72,67,84,56,70
-                ,82,62,73,74,71,59,71,74,57,75,84
-        };
-        int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0;
-        for (int result : sibal) {
-            if(result >= 30 && result <= 42) {
-                count1++;
-            } else if (result >= 43 && result <= 55) {
-                count2++;
-            } else if( result >= 56 && result <= 68) {
-                count3++;
-            } else if (result >= 69 && result <=81) {
-                count4++;
-            } else
-                count5++;
-        }
-        System.out.println(count1);
-        System.out.println(count2);
-        System.out.println(count3);
-        System.out.println(count4);
-        System.out.println(count5);
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main {
+    public String solution(String str) {
+        String answer;
+        char[] s = str.toCharArray(); // 문자열을 문자 배열로 저장
+        int lt = 0; // 첫번 째 문자열을 가리킴
+        int rt = str.length()-1; //마지막 문자 배열 가리킴
+        while (lt < rt) { // 끝 문자와 끝 문자가 만날 때 까지
+            if (!(Character.isAlphabetic(s[lt]))) lt++; //특문인지 검사 후 거짓이면 참이되므로 특문이면 한칸 이동
+            else if(!(Character.isAlphabetic(s[rt]))) rt--;
+            else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        answer=String.valueOf(s); // 문자 배열을 문자열로 변환 시켜줌
+        return answer;
+    }
     public static void main(String[] args) {
-        int human;
-        int[] score = new int[3];
-        int multiply;
-        int result;
-        int[] number = new int[1000];
-        int[] temp = new int[100];
-        int[] tempPlus = new int[100];
+        Main T = new Main();
         Scanner scan = new Scanner(System.in);
-
-        human = scan.nextInt();
-        for (int i = 0; i < human; i++) {
-            number[i] = scan.nextInt();
-            for (int j = 0; j < 3; j++) {
-                score[j] = scan.nextInt();
-            }
-            temp[i] = score[0] * score[1] * score[2];
-            tempPlus[i] = score[0] + score[1] + score[2];
-        }
-        for (int i = 0; i < human-1; i++) {
-            for (int j = 0; j < human - 1; j++) {
-                if (temp[j] > temp[j+1]) {
-                    int im = temp[j];
-                    temp[j] = temp[j + 1];
-                    temp[j + 1] = im;
-                    int im2 = temp[j];
-                    temp[j] = temp[j + 1];
-                    temp[j + 1] = im2;
-                    int imPlus = tempPlus[j];
-                    tempPlus[j] = tempPlus[j + 1];
-                    tempPlus[j + 1] = imPlus;
-                } else if (temp[j] == temp[j+1]) {
-                    if (tempPlus[j] > tempPlus[j+1]) {
-                        int im = number[j];
-                        number[j] = number[j+1];
-                        number[j+1] = im;
-                        int im2 = temp[j];
-                        temp[j] = temp[j+1];
-                        temp[j+1] = im2;
-                        int imPlus = tempPlus[j];
-                        tempPlus[j] = tempPlus[j+1];
-                        tempPlus[j+1] = imPlus;
-                    } else if(temp[j] == temp[j+1] && tempPlus[j] == tempPlus[j+1]){
-                        if(number[j] > number[j+1]) {
-                            int im = number[j];
-                            number[j] = number[j+1];
-                            number[j+1] = im;
-                            int im2 = temp[j];
-                            temp[j] = temp[j+1];
-                            temp[j+1] = im2;
-                            int imPlus = tempPlus[j];
-                            tempPlus[j] = tempPlus[j+1];
-                            tempPlus[j+1] = imPlus;
-                        }
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < 3; i++) {
-            System.out.printf("%d ", number[i]);
-        }
-
+        String str = scan.next();
+        System.out.println(T.solution(str));
     }
 }
