dcce71a620e23254f47e028662855b9656584235
AhnYeongHoo <dksdudgn00@naver.com>
2021-10-15 20:04:35 +0900

    중복 문자 찾기

--- src/Exam.java
+++ src/Exam.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Exam {
     public static void main(String[] args) {
 
 
 
+
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
 import java.util.*;
-public class Main {
-    public String solution(String str) {
-        String answer;
-        char[] s = str.toCharArray(); // 문자열을 문자 배열로 저장
-        int lt = 0; // 첫번 째 문자열을 가리킴
-        int rt = str.length()-1; //마지막 문자 배열 가리킴
-        while (lt < rt) { // 끝 문자와 끝 문자가 만날 때 까지
-            if (!(Character.isAlphabetic(s[lt]))) lt++; //특문인지 검사 후 거짓이면 참이되므로 특문이면 한칸 이동
-            else if(!(Character.isAlphabetic(s[rt]))) rt--;
-            else {
-                char tmp = s[lt];
-                s[lt] = s[rt];
-                s[rt] = tmp;
-                lt++;
-                rt--;
+
+class Main {
+    static int Solution(String str, char t) {
+        int answer = 0; //카운트 할 변수 선언
+        str=str.toUpperCase(); // 문자열도 전부 대문자로 변환
+        t=Character.toUpperCase(t); // 캐릭터 클래스의 대문자변환 메서드
+        for (char temp : str.toCharArray()) { // 향상된 for문 사용해서 문자열을 문자 배열로 temp에 문자로 하나씩 넘김
+            if (temp == t) { // 만약 그 문자와 t가 같다면
+                answer++; // 카운트 증가
             }
         }
-        answer=String.valueOf(s); // 문자 배열을 문자열로 변환 시켜줌
+
         return answer;
+
     }
+
     public static void main(String[] args) {
-        Main T = new Main();
         Scanner scan = new Scanner(System.in);
-        String str = scan.next();
-        System.out.println(T.solution(str));
+        String str = scan.next(); //문자열 입력받기
+        char t = scan.next().charAt(0); //문자열의 첫번 째 인덱스를 받으면 결국 문자
+
+        System.out.print(Solution(str,t)); // 클래스 메서드 호출
+
+
+
     }
 }
